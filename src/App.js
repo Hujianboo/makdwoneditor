8513b4a7442773e5ee6622b5f1573e01e1c80d12
hujianbo <466636709@qq.com>
2020-09-23 15:54:31 +0800

    add FileSearch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FileSearch from './components/FileSearch';
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col-3 bg-danger left-panel">
-          <h1>this is the left</h1>
+        <div className="col bg-danger left-panel">
+          <FileSearch title="myDocument" onFileSearch={(value) => {console.log(value)}}></FileSearch>
         </div>
-        <div className="col-9 bg-primary right-pannel">
+        <div className="col bg-primary right-pannel">
           <h1>this is the right</h1>
         </div>
       </div>
