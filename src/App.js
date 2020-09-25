71d5e7b870e8cb6e946dd3c02689bee3b94021fa
hujianbo <466636709@qq.com>
2020-09-25 22:23:57 +0800

    add FileList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FileSearch from './components/FileSearch';
+import FileList from './components/FileList';
+import defaultFiles from './utils/defaultFiles';
+
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col bg-danger left-panel">
-          <FileSearch title="my Document" onFileSearch={(value) => {console.log(value)}}></FileSearch>
+        <div className="col">
+          <FileSearch  onFileSearch={(value) => {console.log(value)}}></FileSearch>
+          <FileList 
+            files={defaultFiles}
+            onFileClick={(id) => {console.log(id)}}
+            onFileDelete={(id) => {console.log(id)}}
+            onSaveEdit={(id,newValue) => {console.log(id);console.log(newValue);}}
+            ></FileList>
         </div>
         <div className="col bg-primary right-pannel">
           <h1>this is the right</h1>
